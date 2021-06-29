edde72c0cfbc8a022a66869fcdb2d3cedb5a6d98
Jamaal Lawson <maaliemaal@gmail.com>
2021-06-29 17:22:17 -0400

    added a function that will fetch the open weather map api and give a response

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,22 @@ var cityArray = JSON.parse(localStorage.getItem("cities")) || [];
 var  searchHandler = function (event) {
     event.preventDefault();
 
-var cityName = $(".city-name").val().trim();
+var cityName = $(".city-search").val().trim();
+
+function searchWeather(cityName){
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+cityName + '&units=imperial&appid=4e4346890dbabb049a4ba08f09b5e215')
+    .then(function(response) {
+        return response.JSON;
+    })
+    .then(function(data){
+        console.log(data);
+
+        var card = $("<div>").addClass("card").attr("styled: background-color: blue");
+        var cardTitle = $("h2").addClass("cardTitle").text(data.name);
+        $("#weather-info").append(card.append(cardTitle));
+    });
+}
 
 }
+
+$(".submit").on("click", searchHandler);
