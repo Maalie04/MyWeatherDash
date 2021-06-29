cb9012bc5f1b8e03664471c551f77c97afefc8c6
Jamaal Lawson <maaliemaal@gmail.com>
2021-06-29 18:17:04 -0400

    debugged my code and corrected my local storage, and fixed my card class

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,32 @@
 var cityArray = JSON.parse(localStorage.getItem("cities")) || [];
 
 
-var  searchHandler = function (event) {
+var submitHandler = function (event) {
     event.preventDefault();
+    var cityName = $(".city-search").val().trim();
+    console.log(cityName);
 
-var cityName = $(".city-search").val().trim();
+    searchWeather(cityName);
+    cityArray.push(cityName);
+    localStorage.setItem("cities", JSON.stringify(cityArray));
+};
 
-function searchWeather(cityName){
-    fetch('https://api.openweathermap.org/data/2.5/weather?q='+cityName + '&units=imperial&appid=4e4346890dbabb049a4ba08f09b5e215')
-    .then(function(response) {
-        return response.JSON;
-    })
-    .then(function(data){
-        console.log(data);
 
-        var card = $("<div>").addClass("card").attr("styled: background-color: blue");
-        var cardTitle = $("h2").addClass("cardTitle").text(data.name);
-        $("#weather-info").append(card.append(cardTitle));
-    });
+function searchWeather(cityName) {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=imperial&appid=4e4346890dbabb049a4ba08f09b5e215')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+
+            var card = $("<div>").addClass("card").attr("style", "background-color: blue");
+            var cardTitle = $("<h2>").addClass("cardTitle").text(data.name);
+            card.append(cardTitle);
+            $("#weather-info").append(card);
+        });
 }
 
-}
 
-$(".submit").on("click", searchHandler);
+
+$(".submit").on("click", submitHandler);
